25d48ca5a98269020a7d26e704e7300a8f7953f9
Sidarth Venkatesan <sidarth.v14002@gmail.com>
2021-05-04 21:00:16 +0530

    Initial Commit

---
+++ UsedCarApp/UsedCarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsedCarAppApp.swift
+//  UsedCarApp
+//
+//  Created by Sidarth V on 04/05/21.
+//
+
+import SwiftUI
+
+@main
+struct UsedCarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
