97ba5b71f0c06cddf6e91e20c170a820a78eea9e
Sidarth Venkatesan <sidarth.v14002@gmail.com>
2021-05-05 11:00:23 +0530

    Form Page

--- UsedCarApp/UsedCarAppApp.swift
+++ UsedCarApp/UsedCarAppApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct UsedCarAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView{
+                PickerView()
+            }
         }
     }
 }
